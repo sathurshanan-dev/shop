179c8abc2c1ac281619a0b307d6e82833297141f
Sathurshanan Kandhasamy <sath7samy@gmail.com>
2025-09-29 09:01:17 +1000

    update port configuration

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ import connect_db from './config/db.js';
 import { not_found, error_handler } from './middleware/error.js';
 import stripe_route from './routes/stripe.js';
 
+const port = process.env.PORT || 4000;
+
 connect_db();
 
 const app = express();
@@ -34,4 +36,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(not_found);
 app.use(error_handler);
 
-app.listen(4000, console.log('Server running on port 4000'));
+app.listen(
+  4000,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
